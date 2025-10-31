276c9a34dc0ad5bc6b491f9fe64297cecdbcf63a
kagimu <kagimujayp01@gmail.com>
2025-10-31 15:11:31 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import * as path from "path";
 
 export default async () => {
   const plugins = [
     react(),
-    runtimeErrorOverlay(),
   ];
 
   if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
     const { cartographer } = await import("@replit/vite-plugin-cartographer");
     const { devBanner } = await import("@replit/vite-plugin-dev-banner");
 
-    plugins.push(cartographer(), devBanner());
+    plugins.push([cartographer()]);
+    plugins.push([devBanner()]);
   }
 
   return defineConfig({
